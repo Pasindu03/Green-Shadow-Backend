91be9c888bc1b54ab20bba080bb529950a4017c5
Pasindu Yapa <lolmixage03@gmail.com>
2024-11-30 13:33:31 +0530

    Added Exception

---
+++ src/main/java/com/example/greenshadowbackend/exception/DataPersistException.java
@@ -0,0 +1,12 @@
+package com.example.greenshadowbackend.exception;
+
+public class DataPersistException extends RuntimeException {
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+    }
+
+    public DataPersistException(Throwable cause) {
+    }
+}
