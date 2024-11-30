41d08f809efec96defa804267253e9a53b345b2e
Pasindu Yapa <lolmixage03@gmail.com>
2024-11-30 13:43:50 +0530

    Finished Error Status File

--- src/main/java/com/example/greenshadowbackend/customStatusCode/SelectedClassErrorStatus.java
+++ src/main/java/com/example/greenshadowbackend/customStatusCode/SelectedClassErrorStatus.java
@@ -1,7 +1,13 @@
 package com.example.greenshadowbackend.customStatusCode;
 
 import com.example.greenshadowbackend.dto.status.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class SelectedClassErrorStatus implements CropStatus, EquipmentStatus, FieldStaffStatus, FieldStatus, LogStatus, StaffStatus, VehicleStatus, UserStatus, StaffVehicleStatus {
     private int status;
     private String statusMessage;
